69d7f7f9add1584dad90adad4809f73565c524d5
Sailesh Kumar Pasam <144588637+saileshkumar16@users.noreply.github.com>
2025-10-11 00:49:18 +0530

    Add files via upload

---
+++ even_odd.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() 
+{
+    char num = 0;
+    
+    if(num&1)
+    printf("%d is odd number",num);
+    else
+    printf("%d is even number",num);
+    
+    return 0;
+}
