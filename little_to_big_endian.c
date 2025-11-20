63e04f585587d81713d2c60035acd12c0dda571e
Sailesh Kumar Pasam <144588637+saileshkumar16@users.noreply.github.com>
2025-11-20 12:02:35 +0530

    Update little_to_big_endian.c

--- little_to_big_endian.c
+++ little_to_big_endian.c
@@ -1,24 +1,19 @@
 #include <stdio.h>
 #include <stdint.h>
 
-uint32_t little_to_big(uint32_t num)
-{
-    // Swap bytes using bitwise operations
-    uint32_t byte0 = (num & 0x000000FF) << 24; // move byte 0 to byte 3
-    uint32_t byte1 = (num & 0x0000FF00) << 8;  // move byte 1 to byte 2
-    uint32_t byte2 = (num & 0x00FF0000) >> 8;  // move byte 2 to byte 1
-    uint32_t byte3 = (num & 0xFF000000) >> 24; // move byte 3 to byte 0
-
-    return (byte0 | byte1 | byte2 | byte3);
-}
-
 int main()
 {
     uint32_t num = 0x11223344;
 
     printf("Original (Little Endian): 0x%x\n", num);
 
-    uint32_t converted = little_to_big(num);
+    // Swap bytes using bitwise operations
+    uint32_t byte0 = (num & 0x000000FF) << 24; // move byte 0 to byte 3
+    uint32_t byte1 = (num & 0x0000FF00) << 8;  // move byte 1 to byte 2
+    uint32_t byte2 = (num & 0x00FF0000) >> 8;  // move byte 2 to byte 1
+    uint32_t byte3 = (num & 0xFF000000) >> 24; // move byte 3 to byte 0
+
+    uint32_t converted = byte0 | byte1 | byte2 | byte3;
 
     printf("Converted (Big Endian): 0x%x\n", converted);
 
